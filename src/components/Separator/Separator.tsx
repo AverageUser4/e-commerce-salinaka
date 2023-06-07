a32df5bb388c9f6961b50e8096a26e663de1c980
kacper <kacpersloka@o2.pl>
2023-06-07 10:14:00 +0200

    build

--- src/components/Separator/Separator.tsx
+++
@@ -1,9 +0,0 @@
-import css from './Separator.module.css';
-
-export default function Separator({ isVertical, style = {}} : { isVertical?: boolean, style?: Object }) {
-  const className = `${css['separator']} ${isVertical && css['separator--vertical']}`;
-  
-  return (
-    <div className={className} style={style}/>
-  );
-}
