f6b5525d7f8d92f35bafe518bc16c8b7428ac0ca
kacper <kacpersloka@o2.pl>
2023-06-07 10:07:50 +0200

    stuff added

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/salinka/',
   plugins: [react(), svgr()],
   server: {
     open: true,
